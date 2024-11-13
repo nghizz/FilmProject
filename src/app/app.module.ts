e36d4b622023667986ba56a9eac3c1eb29f42ec6
nnhit22 <nnh.it020203@gmail.com>
2024-11-13 21:28:54 +0700

    update index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFilmComponent } from './search-film/search-film.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFilmComponent
+    SearchFilmComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
